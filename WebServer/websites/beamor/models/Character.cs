7b5b33b818934b76f41561f5442563d0a59e9a1a
Mathieu Morrissette <mathieumorrissette@gmail.com>
2016-02-19 21:58:14 -0500

    Add the doc folder

---
+++ WebServer/websites/beamor/models/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer.websites.beamor.models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+
+        public bool Godmode { get; set; }
+        public int Health { get; set;  }
+        public int Strengh { get; set; }
+        public int Defence { get; set;  }
+
+        public int PosX { get; set; }
+        public int PosY { get; set;  }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
