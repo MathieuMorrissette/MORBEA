d981098131aa08555e06b153e6caaa94ef35101f
Kévin Beauséjour <kbswiii@gmail.com>
2016-02-21 11:03:20 -0500

    Added some class and constructor

---
+++ WebServer/websites/beamor/models/monster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServer.websites.beamor.models;
+
+namespace HttpServer.websites.beamor.models
+{
+    class Monster : Character
+    {
+        //constructor
+        public Monster(string name) : base(name)
+        {
+        }
+
+        //constructor
+        public Monster(string name, bool godMode)
+            : base(name, godMode)
+        {
+        }
+
+        //constructor
+        public Monster(string name, int health, int strengh, int defence) 
+            : base(name,health,strengh,defence)
+        {
+        }
+    }
+}
